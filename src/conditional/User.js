6a264c9ddabc4c9f235eaf003d04be895f751c58
shreeji <abc00@gmail.com>
2019-10-18 14:24:59 +0530

    Revert "jatin add"
    This reverts commit a7418dff4626d678b19b875dded74e7723f8c2cb.
    
    # Conflicts:
    #	src/conditional/User.js

--- src/conditional/User.js
+++ src/conditional/User.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class User extends Component {
   render() {
     return (
-      <div>
-        <h1>Welcome Ram</h1>
+      <div><h1>Welcome Ram</h1>
+       <h1>Welcome Raj</h1>
         <button>Logout</button>
       </div>
     )
